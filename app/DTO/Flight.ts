03f8b09aa76fdaa6fe79e82ac7f253df220608f6
KAGMENI Jordan <kagmeni@et.intechinfo.fr>
2023-09-07 16:29:08 +0200

    add price

--- app/DTO/Flight.ts
+++ app/DTO/Flight.ts
@@ -2,4 +2,5 @@ export interface FlightDTO {
   id: string
   airport_departiture: string
   airport_destination: string
+  price: number
 }
--- app/data.ts
+++ app/data.ts
@@ -3,31 +3,37 @@ export const FlightData = [
     id: 'VL001',
     airport_departiture: 'CDG',
     airport_destination: 'JFK',
+    price: 1_000.5,
   },
   {
     id: 'VL002',
     airport_departiture: 'CDG',
     airport_destination: 'DTW',
+    price: 2_000.99,
   },
   {
     id: 'VL003',
     airport_departiture: 'JFK',
     airport_destination: 'DTW',
+    price: 5_000.99,
   },
   {
     id: 'VL004',
     airport_departiture: 'DTW',
     airport_destination: 'JFK',
+    price: 500,
   },
   {
     id: 'VL005',
     airport_departiture: 'JFK',
     airport_destination: 'CDG',
+    price: 3_000.5,
   },
   {
     id: 'VL006',
     airport_departiture: 'DTW',
     airport_destination: 'CDG',
+    price: 10_000.5,
   },
 ]
 
