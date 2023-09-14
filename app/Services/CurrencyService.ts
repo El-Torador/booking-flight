9a5d7659e4a5f3740226e252c40882d701e77a7c
KAGMENI Jordan <kagmeni@et.intechinfo.fr>
2023-09-14 10:21:45 +0200

    update currency feat

--- app/Services/CurrencyService.ts
+++ app/Services/CurrencyService.ts
@@ -1,3 +1,4 @@
+import Logger from '@ioc:Adonis/Core/Logger'
 import Env from '@ioc:Adonis/Core/Env'
 import Redis from '@ioc:Adonis/Addons/Redis'
 import superagent from 'superagent'
@@ -6,13 +7,17 @@ import { DateTime } from 'luxon'
 
 let lastTimeCached: number
 
+export interface Currency {
+  currency: string
+  rate: number
+}
 class CurrencyService {
   private endpoint: string = Env.get('CURRENCY_API')
 
   private setLastTimeCached(value: number): void {
     lastTimeCached = value
   }
-  private parseDataJson(data: {}[]) {
+  private parseDataJson(data: {}[]): Currency[] {
     return data.map((v) => {
       return {
         ...v['$'],
@@ -21,14 +26,25 @@ class CurrencyService {
     })
   }
 
-  public async getCurrencies() {
-    if (lastTimeCached && DateTime.fromMillis(Date.now() - lastTimeCached).hour < 24) {
-      console.info('GET CURRENCY FROM CACHE...')
-      const data = (await Redis.get(lastTimeCached.toString())) as string
-
-      return JSON.parse(data)
+  public getDefaultCurrency(): Currency {
+    return {
+      currency: 'EUR',
+      rate: 1,
     }
-    console.info('RENEW CACHE CURRENCY...')
+  }
+  public async getCurrencies(): Promise<Currency[]> {
+    let tmpData: string | null = null
+
+    if (lastTimeCached && DateTime.fromMillis(Date.now() - lastTimeCached).hour < 24) {
+      Logger.info('GET CURRENCY FROM CACHE...')
+
+      const data = await Redis.get(lastTimeCached.toString())
+      tmpData = data
+
+      if (data) return JSON.parse(data)
+    }
+
+    Logger.info('RENEW CACHE CURRENCY...')
 
     try {
       const response = await superagent
@@ -36,21 +52,41 @@ class CurrencyService {
         .accept('xml')
         .parse(xml2jsParser)
         .buffer(true)
+      if (!response.ok) throw new Error('Error Occured when getting currencies.')
       const data = response.body
 
+      const currencies = this.parseDataJson(data['gesmes:Envelope']['Cube'][0]['Cube'][0]['Cube'])
+
+      currencies.unshift(this.getDefaultCurrency())
+
+      if (lastTimeCached && tmpData) await Redis.del(lastTimeCached.toString())
       this.setLastTimeCached(DateTime.now().toMillis())
 
-      const value = this.parseDataJson(
-        data['gesmes:Envelope']['Cube'][0]['Cube'][0]['Cube']
-      ) as Record<string, {}>[]
+      await Redis.set(lastTimeCached.toString(), JSON.stringify(currencies))
 
-      await Redis.set(lastTimeCached.toString(), JSON.stringify(value))
-
-      return value
+      return currencies
     } catch (error) {
-      console.error(error)
-      return JSON.parse((await Redis.get(lastTimeCached.toString())) as string)
+      Logger.error(error)
+
+      const currencies = await Redis.get(lastTimeCached.toString())
+      if (!currencies) throw new Error(error?.message)
+
+      return JSON.parse(currencies) as Currency[]
     }
+  }
+
+  public async getCurrenciesMapper(): Promise<Map<string, Currency>> {
+    const currencies = await this.getCurrencies()
+    const map = new Map<string, Currency>()
+    currencies.forEach((currency) => {
+      map.set(currency.currency, currency)
+    })
+
+    return map
+  }
+
+  public searchCurrency(currencyMap: Map<string, Currency>, currency: string): Currency {
+    return currencyMap.get(currency.toUpperCase()) ?? this.getDefaultCurrency()
   }
 }
 
